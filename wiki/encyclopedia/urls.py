652b8a14843eb3672087763a0413ffdbea4d88a3
Mr. Baker <ojibweeagle@outlook.com>
2025-12-13 22:11:45 -0600

    urls

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index")
     path("<str:title>", views.entry, name="entry")
+    path("edit/<str:title>", views.edit_page, name="edit_page"),
+    
 ]
