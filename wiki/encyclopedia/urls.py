365ae3bcf515a8876183fb57f60fef05adf809d3
Mr. Baker <ojibweeagle@outlook.com>
2025-12-13 21:00:43 -0600

    Add url

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index")
-    path("<str:title>", views.entry, name=entry)
+    path("<str:title>", views.entry, name="entry")
 ]
