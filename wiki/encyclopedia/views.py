0b1708c53c8c32c5378afc02697359a86284ed37
Mr. Baker <ojibweeagle@outlook.com>
2025-12-13 20:50:22 -0600

    Add entry to views

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -8,3 +8,18 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    content = util.get_entry(title)
+    if content is None:
+        return render(request, "encyclopedia/error.html", {
+          "error_title": "Page Not Found",
+          "error_message": f"The page '{title}' does not exist",
+          "title": title,  
+        })
+    
+    html_content = markdown.markdown(content)
+    return render(request, "encyclopedia/error.html" {
+        "title": title,
+        "content": util.markdown_to_html(content),
+    })
+                      
