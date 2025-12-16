fb08efa434dd001d7498f4c9bc63ac2b5f5eb5b4
Mr. Baker <ojibweeagle@outlook.com>
2025-12-15 21:38:02 -0600

    random_page

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
     path("wiki/<str:title>/edit", views.edit, name="edit"),
-    path("random", views.random, name="random"),
+    path("random", views.random_page, name="random"),
     path("new", views.new, name="new"),
     path("search", views.search, name="search"),
 ]
