f2d09b36e8c1618687af99c2337c2d7719bc013f
Mr. Baker <ojibweeagle@outlook.com>
2025-12-15 18:11:26 -0600

    UD urls

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
-    path("<str:title>", views.entry, name="entry")
+    path("", views.index, name="index"),
+    path(r"wiki/<str:title>", views.entry, name="entry"),
     path("<str:title>/edit", views.edit, name="edit"),
     
 ]
