1c2bd272976fa9eb325ba6e2f13c8d0683d6a3fb
wildrice715 <ojibweeagle@outlook.com>
2026-03-18 16:44:37 -0500

    urls

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry, name="entry"),
-    path("search", views.search, name="search"),
+    path("wiki/<str:entry>", views.wiki, name="wiki"),
+    path("search/", views.search, name="search"),
     path("create", views.create, name="create"),
-    path("edit/<str:title>", views.edit, name="edit"),
+    path("edit/<str:entry>", views.edit, name="edit"),
     path("random", views.random, name="random"),
 ]
