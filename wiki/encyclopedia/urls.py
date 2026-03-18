d7a545fcaa68e2eafcfe0a7f05eb4043881be497
Mr. Baker <ojibweeagle@outlook.com>
2026-03-17 22:21:45 -0500

    urls entry

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:entry>", views.entry, name="entry"),
+    path("wiki/<str:title>", views.entry, name="entry"),
     path("search", views.search, name="search"),
     path("random", views.random, name="random"),
     path("create", views.create, name="create"),
